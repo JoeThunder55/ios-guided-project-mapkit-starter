c99ae6fb9b1d27a772ecec386138a45b221a2960
Aaron Haughton <dreamcast2000@outlook.com>
2020-05-13 21:51:21 -0400

    tried adding the user's location on a map

--- Quakes/Quakes/EarthquakesViewController.swift
+++ Quakes/Quakes/EarthquakesViewController.swift
@@ -13,9 +13,17 @@ class EarthquakesViewController: UIViewController {
 		
 	// NOTE: You need to import MapKit to link to MKMapView
 	@IBOutlet var mapView: MKMapView!
-	
+    private var userTrackingButton: MKUserTrackingButton!
+    
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
+        userTrackingButton = MKUserTrackingButton(mapView: mapView)
+        userTrackingButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userTrackingButton)
+        
+        NSLayoutConstraint.activate([
+            userTrackingButton.leadingAnchor.constraint(equalTo: mapView.leadingAnchor, constant: 20),
+            userTrackingButton.bottomAnchor.constraint(equalTo: userTrackingButton.bottomAnchor, constant: 20)
+        ])
 	}
 }
